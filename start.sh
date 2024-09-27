2b32949b180cf9d6b0fab5db65063fad30627605
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-27 12:17:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+rm -r downloads
 python -m venv myenv  # Create a virtual environment (if not already created)
 source myenv/bin/activate  # Activate the virtual environment
 python -m ensurepip --upgrade  # Ensure pip is installed and upgrade it
