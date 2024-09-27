6e3ee334d4ad4440118f530ae2ab635492d394eb
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-09-27 12:03:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -50,5 +50,6 @@ def get_info(supabase, table_name, url):
 # Get all URLs from the Supabase table
 def get_raw_url(supabase, table_name):
     documents = find_documents(supabase, table_name)
+    print(documents)
     urls = [doc["URL"] for doc in documents]
     return urls
